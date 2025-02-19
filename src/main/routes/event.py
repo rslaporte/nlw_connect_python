0c57fa7952bc9b02b9e49ef1fbb603a3eeaf2a13
rslaporte <rafanex@gmail.com>
2025-02-19 19:14:27 -0300

    Implementando Validators das rotas

--- src/main/routes/event.py
+++ src/main/routes/event.py
@@ -1,7 +1,16 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 
 event_route_bp = Blueprint("event_route", __name__)
 
+from src.validators.events_creator_validator import events_creator_validator
+
+from src.https_types.http_response import HttpResponse
+from src.https_types.http_request import HttpRequest
+
 @event_route_bp.route("/event", methods=["POST"])
 def create_new_event():
-    return jsonify({"estou": "aqui"}), 201
+    events_creator_validator(request)
+    http_request = HttpRequest(body=request.json)
+    http_response = HttpResponse(body={"estou": "aqui"}, status_code=201)
+
+    return jsonify(http_response.body), http_response.status_code
---
+++ src/validators/subscriber_creator_validator.py
@@ -0,0 +1,19 @@
+from cerberus import Validator
+
+def subscribers_creator_validator(request: any):
+    body_validator = Validator({
+       "data": {
+           "type": "dict",
+           "schema": {
+               "nome": { "type" : "string" , "required" : True, "empty": False },
+               "email": { "type" : "string", "required": True, "empty": False },
+               "link": { "type": "string", "empty": False },
+               "evento_id": { "type" : "integer", "required": True, "empty": False }
+           }
+       } 
+    })
+
+    response = body_validator.validate(request.json)
+    
+    if(response is False):
+        print(body_validator.errors)
